8b7ad70b7ecb782f4016e11281d60982a1f93ef7
zhwang0 <zhihaowang619@gmail.com>
2026-08-18 19:37:20 -0400

    Add publication resource links

--- assets/js/about.js
+++ assets/js/about.js
@@ -56,7 +56,7 @@
     }
     publications.setAttribute("data-load-started", "true");
 
-    fetch("/publications/?v=20260818-rse1")
+    fetch("/publications/?v=20260818-resources1")
       .then(function (response) {
         if (!response.ok) {
           throw new Error("Unable to load publications");
